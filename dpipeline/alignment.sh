0b1f07375eb3dfa618643c9764a5ba1a214c37bd
CoditoDTU <rogall@dtu.dk>
2024-02-06 17:14:18 -0500

    bugfixed alignment.sh | results from the .sh file were created | .hmm file copied in dpipeline for .sh functionality

--- dpipeline/alignment.sh
+++ dpipeline/alignment.sh
@@ -8,15 +8,15 @@ perform_msa() {
     local prefix="$3"
     local cluster_number="$4"
 
-    local input_file="$folder_path/${prefix}_clu_seq.$cluster_number"
-    local output_file="$folder_path/${prefix}_MSA_cluster${cluster_number}.fasta"
+    local input_file="$folder_path${prefix}_clu_seq.$cluster_number"
+    local output_file="$folder_path${prefix}_MSA_cluster${cluster_number}.fasta"
 
     echo "Processing file: $input_file"
     echo "Input file: $input_file"
     echo "Output file: $output_file"
 
     # Placeholder for the actual clustalo command
-    # clustalo -i "$input_file" --hmm-in "$hmm_file" -o "$output_file"
+    clustalo -i "$input_file" --hmm-in "$hmm_file" -o "$output_file"
 
     echo "MSA for cluster $cluster_number completed."
 }
@@ -24,14 +24,15 @@ perform_msa() {
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 -hmm <hmm_file> -r <results_folder> -p <prefix>"
+    echo "Usage: $0 -hmm <hmm_file> -i <input_results_folder> -p <prefix>"
     echo "Options:"
     echo "  -h, --hmm        Path to the HMM file"
-    echo "  -i, --input      Path to the results folder"
+    echo "  -i, --input      Path to the results folder ex: data/ do not forget the /"
     echo "  -p, --prefix     Prefix for file matching"
     echo "  -help            Display this help message"
     exit 0  # Exit successfully after showing usage information
 }
+
 
 # Parse command-line options
 while getopts "h:i:p:" opt; do
@@ -60,17 +61,20 @@ fi
 
 # Check if the required options are provided
 if [ -z "$hmm_file" ] || [ -z "$folder_path" ] || [ -z "$prefix" ]; then
-    echo "Error: All options (-hmm, -r, -p) must be provided."
+    echo "Error: All options (-hmm, -i, -p) must be provided."
     usage
 fi
 
 # Loop through each file in the folder with the pattern ${prefix}_clu_seq.*
 for file in "$folder_path/${prefix}_clu_seq".*; do
-    # Check if the file matches the expected pattern
-    if [[ $file =~ ${prefix}_clu_seq\.([0-9]+)$ ]]; then
-        cluster_number="${BASH_REMATCH[1]}"
+    # Extract cluster number from the file name
+    cluster_number=$(basename "$file" | grep -oP "(?<=${prefix}_clu_seq\.)[0-9]+")
+    
+    # Check if the cluster number is valid (non-empty and numerical)
+    if [[ -n "$cluster_number" ]] && [[ "$cluster_number" =~ ^[0-9]+$ ]]; then
         perform_msa "$folder_path" "$hmm_file" "$prefix" "$cluster_number"
     else
         echo "Skipping file $file as it does not match the expected pattern."
     fi
 done
+
