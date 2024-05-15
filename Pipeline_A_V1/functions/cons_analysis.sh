e460b1f31651ba107076e159cd423f7322e8946b
Ubuntu <codito@coditomachine.e0y0jbgvynquteq5daaxv3mvfc.ax.internal.cloudapp.net>
2024-05-15 20:50:04 +0000

    removing files needed for new pipe A targeted run

--- Pipeline_A_V1/functions/cons_analysis.sh
+++ Pipeline_A_V1/functions/cons_analysis.sh
@@ -13,10 +13,13 @@ usage() {
 prefix="tryp"  # Default prefix
 
 # Parse command-line options
-while getopts ":i:p:" opt; do
+while getopts ":i:o:p:" opt; do
     case $opt in
         i)
-            folder_path="$OPTARG"   # Store the provided folder path
+            input_folder_path="$OPTARG"   # Store the provided folder path
+            ;;
+        o)
+            output_folder_path="$OPTARG"   # Store the provided output folder path for csv results
             ;;
         p)
             prefix="$OPTARG"   # Store the provided prefix
@@ -42,7 +45,7 @@ for file in "$folder_path"/"${prefix}"_clu_seq*_og_aligned.fasta; do
         cluster_number=$(echo "$file" | grep -oP "(?<=${prefix}_clu_seq)[0-9]+")
 
         # Run the Python script for the current file
-        python3 functions/conserved_seq.py -i "$file" -o "$folder_path/${prefix}_clu_${cluster_number}_A_cs.csv"
+        python3 functions/conserved_seq.py -i "$file" -o "$output_folder_path/${prefix}_clu_${cluster_number}_A_cs.csv"
 
         echo "Conservation analysis executed for $file"
     else
